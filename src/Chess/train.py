5adae915be6b8f4bfc1e5bc585b79cbc9e428373
mkrum <mrkrumdick@gmail.com>
2018-04-08 23:36:40 -0400

    updated logging

--- src/Chess/train.py
+++ src/Chess/train.py
@@ -12,7 +12,56 @@ import time
 import chess.uci
 import os
 from util import get_input
-from chess_util import test_random, to_pgn
+
+def to_pgn(board):
+    '''
+    Converts a board object into a pgn file
+    '''
+    game = chess.pgn.Game()
+    moves = list(board.move_stack)
+    node = game.add_variation(moves[0])
+
+    for i in range(1, len(moves)):
+        node = node.add_variation(moves[i])
+
+    return str(game)
+
+def test_random(model, size, history):
+    '''
+    tests the model against a random opponent
+    '''
+    wins = 0
+    draws = 0
+
+    pgn = []
+    for i in range(size):
+        print('{}/{}'.format(i + 1, size), end='\r')
+        board = chess.Board()
+        boards = []
+        player = 1
+        while not board.is_game_over(claim_draw=True):
+
+            if player == 1:
+                move = select(boards, model, history)
+            else:
+                move = random.choice(list(board.legal_moves))
+
+            player = (player % 2) + 1
+
+            board.push(move)
+            boards.append(copy.copy(board))
+
+        res = board.result()
+
+        if res == '1-0':
+            wins += 1
+        else:
+            draws += 1
+
+        pgn.append(to_pgn(board))
+
+    print('Wins: {} Draws: {} Losses: {}'.format(wins, draws, size - (wins + draws)))
+    return pgn
 
 
 def main(args):
@@ -25,12 +74,12 @@ def main(args):
     gamma = args.gamma
     for it in range(args.iter):
         if it > 0:
-            f = open(str(it), 'w')
+            f = open(args.path + '/' + str(it), 'w')
             f.write('Gamma: {}\n'.format(gamma))
             f.write('Average Loss: {}\n'.format(sum(losses) / len(losses)))
             f.write('Time (Minutes): {}\n'.format(((end - start) / 60)))
             f.write('\n\n')
-            pgn = test_random(model, 2)
+            pgn = test_random(model, 2, args.history)
             for g in pgn:
                 f.write(g)
                 f.write('\n\n')
@@ -73,16 +122,17 @@ def main(args):
             p.terminate()
             p.join()
 
-        gamma *= .9
-        model.save()
+        gamma *= args.decay
+        model.save(args.path + '/.modelprog')
         end = time.time()
 
-    model.save()
-    f = open('final', 'w')
+
+    model.save(args.path + '/.modelprog')
+    f = open(args.path + '/final', 'w')
     f.write('Gamma: {}\n'.format(gamma))
     f.write('Average Loss: {}\n'.format(sum(losses) / len(losses)))
     f.write('\n\n')
-    pgn = test_random(model, 2)
+    pgn = test_random(model, 2, args.history)
     for g in pgn:
         f.write(g)
         f.write('\n\n')
@@ -108,14 +158,21 @@ if __name__ == '__main__':
     parser.add_argument('--quiet', dest='quiet', action='store_const',
                         const=True, default=False, help='Repress progress output')
 
-    parser.add_argument('--load', metavar='e', type=str, default=None,
+    parser.add_argument('--load', metavar='l', type=str, default=None,
                     help='Load a pre existing file')
 
-    parser.add_argument('--gamma', metavar='e', type=float, default=1.0,
+    parser.add_argument('--gamma', metavar='g', type=float, default=1.0,
                     help='Exploration parameter')
 
-    parser.add_argument('--history', metavar='e', type=int, default=8,
+    parser.add_argument('--decay', metavar='d', type=float, default=0.9,
                     help='Exploration parameter')
+
+    parser.add_argument('--history', metavar='h', type=int, default=8,
+                    help='Number of previous boards to include in the input')
+
+    savepath = 'res/' + '-'.join(time.ctime().split())
+    parser.add_argument('--path', metavar='p', type=str, default=savepath,
+                    help='Experiment path')
 
     args = parser.parse_args()
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
@@ -129,5 +186,12 @@ if __name__ == '__main__':
     else:
         print('Model not found: {}'.format(args.model))
         exit()
-        
+    
+    os.mkdir(args.path)
+    settings_file = open(args.path + '/settings.txt', 'w')
+    for arg in vars(args):
+        settings_file.write('{} {}\n'.format(arg, getattr(args, arg)))
+
+    settings_file.close()
+
     main(args)
